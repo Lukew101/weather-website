c2f427fa7ec3f81223fda10e8ac7eea299d232e7
Lukew101 <109954692+Lukew101@users.noreply.github.com>
2023-02-16 14:28:26 +0100

    Update threeDayForecast - hourly temp

--- js/components/threeDayForecast.js
+++ js/components/threeDayForecast.js
@@ -2,21 +2,32 @@ export function threeDayForecast(results,contentAppend){
     for(let i = 0; i < results.forecast.forecastday.length; i++){
         let dayData = results.forecast.forecastday[i];
 
-        displayHourlyTemp(dayData.hour);
-
         const cardDate = document.createElement("h2");
         const tempEachHour = document.createElement("p");
         
         cardDate.innerHTML = `${results.forecast.forecastday[i].date}`;
-        tempEachHour.innerHTML = `${dayData.hour.temp_c}`
 
         contentAppend.appendChild(cardDate);
         contentAppend.appendChild(tempEachHour);
+        displayHourlyTemp(dayData.hour,contentAppend);
     }
 }
 
-function displayHourlyTemp(results){
+function displayHourlyTemp(results,contentAppend){
     for(let i = 0; i < results.length; i++){
-        let results = results[i];
+        console.log(results[i].temp_c);
+        console.log(results[i].time);
+
+        const hourlyTimeHeader = document.createElement("p");
+        hourlyTimeHeader.innerHTML += results[i].time;
+
+        contentAppend.appendChild(hourlyTimeHeader);
+
+        const hourlyDegreeHeader = document.createElement("p");
+        hourlyDegreeHeader.innerHTML += results[i].temp_c;
+
+        contentAppend.appendChild(hourlyDegreeHeader);
     }
 }
+
+// work on this
